9335bf991cec69951a7f5cdfd2be2585cf140bd8
gitteration <show10033@naver.com>
2022-04-09 14:49:50 +0900

    elementaryArithmetic sccuess

---
+++ baekjoon/step/IOAndOperation/elementaryArithmetic.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+
+(function(){
+	rl.on('line', function (line) {
+		line = line.split(' ');
+		const A = Number(line[0])
+		const B = Number(line[1])
+		const plus = A + B;
+		const minus = A - B;
+		const multiply = A * B;
+		const division =  Math.trunc(A / B);
+		const percent = A % B;
+		console.log(plus);
+		console.log(minus);
+		console.log(multiply);
+		console.log(division);
+		console.log(percent);
+		rl.close();
+	})
+})()
