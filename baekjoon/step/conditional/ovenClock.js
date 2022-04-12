26d0d1257b0eec954eaa2f026a747a1f198eeff1
SANGHWA LEE <show10033@naver.com>
2022-04-13 01:57:41 +0900

    question ovenClock success

--- baekjoon/step/conditional/ovenClock.js
+++ baekjoon/step/conditional/ovenClock.js
@@ -39,26 +39,25 @@ const {stdin: input, stdout: output} = require('process');
 const rl = readline.createInterface({input, output});
 
 (async function(){
+	const limit = 3;
 	let numbers = [];
 	for await(const line of rl){
 		numbers.push(...line.trim().split(' ').map(num => Number(num)));
-		if(numbers.length >= 3) break;
+		if(numbers.length >= limit) break;
 	}
 
 	let [hour, minute, add_minute] = numbers;
-	if(!(hour >= 0 && hour <= 23) || !(minute >= 0 && minute <= 59)){	//0 ≤ hour ≤ 23, 0 ≤ minute ≤ 59 validate
+	if(!(hour >= 0 && hour <= 23) || !(minute >= 0 && minute <= 59) || !(add_minute >= 0 && add_minute <= 1000)){	//0 ≤ hour ≤ 23, 0 ≤ minute ≤ 59 validate
 		console.log('올바른 시간을 입력해주세요');
 		rl.close();
-	};
-	console.log(hour, minute, add_minute);
-	minute += add_minute;
-	if(minute <= 60){
-		minute = 0
-		hour += 1;
-		minute += add_minute - minute;
+		return;
 	}
-	if(hour < 0){
-		hour = 24 + hour;
+	minute += add_minute;
+	if(minute >= 60){
+		const division = Math.floor(minute / 60);
+		hour += division;
+		minute = minute - 60 * division;
+		hour = hour >= 24 ? hour - 24 : hour;
 	}
 	console.log(`${hour} ${minute}`);
 })();
