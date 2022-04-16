ca0ce30db0f86e53f94b55078c6007f06f033304
SANGHWA LEE <show10033@naver.com>
2022-04-16 18:51:37 +0900

    question plusAndMinus3 success

---
+++ baekjoon/step/iteration/plusAndMinus3.js
@@ -0,0 +1,45 @@
+/**
+- 문제 -
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+- 입력 -
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+- 출력 -
+각 테스트 케이스마다 A+B를 출력한다.
+
+- 예제 입력 1 -
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+- 예제 출력 1 -
+2
+5
+7
+17
+7
+*/
+
+// 풀이
+const readline = require('readline');
+const {stdin: input, stdout:output} = require('process');
+const rl = readline.createInterface({input, output});
+
+(async function(){
+	let numbers = [];
+	for await(const line of rl){
+		numbers.push(line.trim().split(' ').map(num=> Number(num)));
+		const line_limit = numbers[0][0] + 1;
+		if(line_limit <= numbers.length){
+			numbers.shift(); break;
+		}; 
+	}
+	
+	for(let i in numbers){
+		console.log(`${numbers[i][0] + numbers[i][1]}`);
+	}
+})();
