9eec10963e9609e8c26647c7e72b445361a78936
SANGHWA LEE <show10033@naver.com>
2022-04-19 13:29:34 +0900

    question plusVersion5 success

---
+++ baekjoon/step/iteration/plusVersion5.js
@@ -0,0 +1,42 @@
+/**
+- 문제 -
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+- 입력 -
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+입력의 마지막에는 0 두 개가 들어온다.
+
+- 출력 -
+각 테스트 케이스마다 A+B를 출력한다.
+
+- 예제 입력 1 -
+1 1
+2 3
+3 4
+9 8
+5 2
+0 0
+
+- 예제 출력 1 -
+2
+5
+7
+17
+7 
+*/
+
+const readline = require('readline');
+const {stdin: input, stdout: output} = require('process');
+const rl = readline.createInterface({input, output});
+let numbers = [];
+rl.on('line', (line)=> {
+	if(line.trim() === '0 0') rl.close();
+	numbers.push(line.trim().split(' ').map(num => Number(num)));
+}).on('close', ()=> {
+	let result = ``;
+	for(let index in numbers){
+		result += `${numbers[index][0] + numbers[index][1]}\n`
+	}
+	console.log(`${result.trim()}`);
+});
