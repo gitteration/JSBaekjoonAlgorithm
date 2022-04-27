a348ff681d3e65ce1092ccbb97e6fed188f94310
SANGHWA LEE <show10033@naver.com>
2022-04-27 23:49:32 +0900

    question remain success

--- baekjoon/step/oneDimensionArray/remain.js
+++ baekjoon/step/oneDimensionArray/remain.js
@@ -57,3 +57,20 @@
 6
 각 수를 42로 나눈 나머지는 39, 40, 41, 0, 1, 2, 40, 41, 0, 1이다. 서로 다른 값은 6개가 있다.
  */
+
+const readline = require('readline');
+const {stdin: input, stdout: output} = require('process');
+const rl = readline.createInterface({input, output});
+
+const limit = 10;
+let numbers = new Set();
+let count = 0;
+rl.on('line', (line)=> {
+	const remain = Number(line.trim()) % 42;
+	numbers.add(remain);
+	count ++;
+	if(count >= limit){
+		console.log(numbers.size)
+		rl.close();
+	};
+});
