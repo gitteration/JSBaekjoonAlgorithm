07a2b967ba65ddffbcf7ee2c92c3b07ad0106d63
SANGHWA LEE <show10033@naver.com>
2022-04-29 00:28:13 +0900

    question average ongoing

--- baekjoon/step/oneDimensionArray/average.js
+++ baekjoon/step/oneDimensionArray/average.js
@@ -74,21 +74,22 @@ const readline = require('readline');
 const {stdin: input, stdout: output} = require('process');
 const rl = readline.createInterface({input, output});
 
-let numbers;
+let scores;
 let limit;
 rl.on('line', (line) => {
-	console.log(limit)
-
 	if(!limit){
 		limit = Number(line);
 	}else{
-		numbers = line.trim().split(' ').map(num => Number(num));
-	}
-	if(limit === numbers.length){
-		rl.close();
+		scores = line.trim().split(' ').map(num => Number(num));
+		limit === scores.length ? rl.close() : console.log(`입력하신 배열의 크기가 다릅니다. 입력하신 배열의 크기는 ${limit} 입니다`);
 	}
 }).on('close', ()=> {
-	//console.log(numbers)
+	const maximum = Math.max(...scores);
+	let result = 0;
+	scores = scores.map(score => (score / maximum) * 100);
+	for(let index in scores){
+		result += scores[index];
+	}
+	result = parseFloat(result / limit);
+	console.log(result);
 })
-
-
