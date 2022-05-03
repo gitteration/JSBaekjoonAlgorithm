badd37d3e816f2e917003b1ea2c9cf900e791e7f
SANGHWA LEE <show10033@naver.com>
2022-05-03 21:12:06 +0900

    problem content change to link

--- baekjoon/step/IOAndOperation/multiply.js
+++ baekjoon/step/IOAndOperation/multiply.js
@@ -1,22 +1,4 @@
-/**
-- 문제 -
-두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
-
-- 입력 -
-첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-
-- 출력 -
-첫째 줄에 A*B를 출력한다.
-
-- 예제 입력 -  
-2 2
-
-- 예제 출력 -  
-4
-
-*/
-
-// 풀이
+// 문제 링크 https://www.acmicpc.net/problem/10998
 
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
