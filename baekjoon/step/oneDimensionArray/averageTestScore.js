a24ef451411332f0e8f7ca0729e7fae4f3e03419
SANGHWA LEE <show10033@naver.com>
2022-05-12 17:29:55 +0900

    qestion averageTestScore success

--- baekjoon/step/oneDimensionArray/averageTestScore.js
+++ baekjoon/step/oneDimensionArray/averageTestScore.js
@@ -5,38 +5,70 @@ const {stdin: input, stdout: output} = require('process');
 const rl = readline.createInterface({input, output});
 
 function Info(){
-	let score = [];
-	let line_limit;
+	this.score = [];
+	this.number_of_students = [];
+	this.line_limit = 0;
 }
-
-Info.prototype.setLineLimit = (line) => {
-	this.score = line;
+Info.prototype.setLineLimit = function(line) {
+	this.line_limit = line;
 }
-Info.prototype.getLineLimit = () => {
+Info.prototype.getLineLimit = function(){
+	return this.line_limit;
+}
+Info.prototype.setScore = function(line){
+	this.score.push(line);
+}
+Info.prototype.getFullScore = function(){
 	return this.score;
 }
-
+Info.prototype.getScore = function(index){
+	return this.score[index]
+}
+Info.prototype.getScoreLength = function(){
+	return this.score.length;
+}
+Info.prototype.setNumberOfStudents = function(line){
+	this.number_of_students.push(line);
+}
+Info.prototype.getNumberOfStudents = function(index){
+	return this.number_of_students[index];
+}
+Info.prototype.sum = function(acc, curr){
+	return acc + curr;
+}
+Info.prototype.average = function(){
+	return 
+}
+const info = new Info();
 
 rl.on('line', (line) => {
-	const info = new Info();
 	let line_limit = info.getLineLimit();
-	if(!line_limit){
+	if(line_limit === 0){
 		info.setLineLimit(Number(line));
 	}else{
 		line = line.trim().split(' ').map(num => Number(num));
-		score.push(line);
-		line_limit === score.length ? rl.close() : undefined;
+		info.setNumberOfStudents(line[0]);
+		line.shift(); 
+		info.setScore(line);
+		let score_length = info.getScoreLength();
+		line_limit === score_length ? rl.close() : undefined;
 	}
 }).on('close', ()=> {
 	let result = '';
-	for(let index in score){
-		let sum = 0;
+	let scores = info.getFullScore();
+	for(let index in scores){
+		let score = info.getScore(index);
+		let number_of_students = info.getNumberOfStudents(index);
+		let sum = score.reduce(info.sum, 0);
+		let score_average = parseFloat((sum / number_of_students));
 		let average = 0;
-		let number_of_socre = 0;
-		score[index].forEach(element => {
-			number_of_socre === 0 ? number_of_socre = element : sum += element;
-		});
-		average = parseFloat(sum / number_of_socre);
+		let count = 0;
+		for(let element of score){
+			if(element > score_average){
+				count ++;
+			} 
+		}
+		average = ((count / number_of_students) * 100).toFixed(3);
 		result += `${average}%\n`;
 	}
 	console.log(result);
