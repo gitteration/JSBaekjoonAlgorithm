809691a15ffe30b795b964535cfec4d4abe361b6
SANGHWA LEE <show10033@naver.com>
2022-05-25 14:40:41 +0900

    question sort success

---
+++ programmers/highScoreKit/sort/KNumber.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    let answer = [];
+    for(const index in commands){
+        const [i,j,k] = [commands[index][0]-1, commands[index][1], commands[index][2]-1];
+        const target_value = array.slice(i,j).sort((a,b)=>{return a-b})[k];
+        answer.push(target_value);
+    }
+    return answer;
+}
